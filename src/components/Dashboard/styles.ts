0bd0c689459a8616d5ec26d173b201c212b4227f
Rodolfo Mori <rod_dolpho@hotmail.com>
2020-12-07 12:45:16 -0300

    fix: icon Money name

--- src/components/Dashboard/styles.ts
+++ src/components/Dashboard/styles.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-import Money from '../../icons/money';
+import Money from '../../icons/Money';
 
 export const Container = styled.div`
   box-shadow: 0px 16px 32px rgba(30, 42, 50, 0.08);
